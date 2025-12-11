517c2817a16f893b1cc3a1f536cb7c7a58f37c33
Siddharth Jha <sidjha57@gmail.com>
2025-12-12 02:07:58 +0530

    fix: added base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/evamed-landing/' : '/',
   plugins: [
     vue({
       // Use runtime-only build (smaller, no template compiler)
