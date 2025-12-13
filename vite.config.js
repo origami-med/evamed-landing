c7ad7e5ca0d1001ec46a4b92379a8ce08a68b76b
Siddharth Jha <sidjha57@gmail.com>
2025-12-13 23:05:46 +0530

    chore: added CNAME

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/evamed-landing/' : '/',
+  base: '/',
   plugins: [
     vue({
       // Use runtime-only build (smaller, no template compiler)
