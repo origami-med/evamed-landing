1f919b2f84f0fae45ecc450445e663ec9e5793a4
Siddharth Jha <sidjha57@gmail.com>
2025-12-23 13:12:49 +0530

    chore: added vercel analytics

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import './styles/input.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 createApp(App).use(router).mount('#app');
